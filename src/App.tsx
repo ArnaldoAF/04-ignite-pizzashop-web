e5fb0044927563a33db6ff08ed14f4c3acc976a1
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2024-10-02 14:56:54 -0300

    fix: prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "./components/ui/button";
 export function App() {
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex  items-center h-screen justify-center">
 
       <Button>Enviar</Button>
     </div>
